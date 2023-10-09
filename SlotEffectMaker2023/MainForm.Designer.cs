7d4a5047f849e3569393bd5227f817285eb82caa
sin spark <spaaaark.sin120@gmail.com>
2023-10-09 10:23:06 +0900

    [ADD]初期4項目編集用ボタン配置
    アクションはこれから

--- SlotEffectMaker2023/MainForm.Designer.cs
+++ SlotEffectMaker2023/MainForm.Designer.cs
@@ -29,14 +29,58 @@ namespace SlotEffectMaker2023
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnValSet = new System.Windows.Forms.Button();
+            this.btnSoundID = new System.Windows.Forms.Button();
+            this.btnSoundPlayData = new System.Windows.Forms.Button();
+            this.btnUserTimer = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnValSet
+            // 
+            this.btnValSet.Location = new System.Drawing.Point(10, 10);
+            this.btnValSet.Name = "btnValSet";
+            this.btnValSet.Size = new System.Drawing.Size(140, 30);
+            this.btnValSet.TabIndex = 0;
+            this.btnValSet.Text = "ユーザ変数設定";
+            this.btnValSet.UseVisualStyleBackColor = true;
+            // 
+            // btnSoundID
+            // 
+            this.btnSoundID.Location = new System.Drawing.Point(10, 90);
+            this.btnSoundID.Name = "btnSoundID";
+            this.btnSoundID.Size = new System.Drawing.Size(140, 30);
+            this.btnSoundID.TabIndex = 0;
+            this.btnSoundID.Text = "音源設定";
+            this.btnSoundID.UseVisualStyleBackColor = true;
+            // 
+            // btnSoundPlayData
+            // 
+            this.btnSoundPlayData.Location = new System.Drawing.Point(10, 130);
+            this.btnSoundPlayData.Name = "btnSoundPlayData";
+            this.btnSoundPlayData.Size = new System.Drawing.Size(140, 30);
+            this.btnSoundPlayData.TabIndex = 0;
+            this.btnSoundPlayData.Text = "音再生タイミング設定";
+            this.btnSoundPlayData.UseVisualStyleBackColor = true;
+            // 
+            // btnUserTimer
+            // 
+            this.btnUserTimer.Location = new System.Drawing.Point(10, 50);
+            this.btnUserTimer.Name = "btnUserTimer";
+            this.btnUserTimer.Size = new System.Drawing.Size(140, 30);
+            this.btnUserTimer.TabIndex = 0;
+            this.btnUserTimer.Text = "ユーザタイマ設定";
+            this.btnUserTimer.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.btnSoundPlayData);
+            this.Controls.Add(this.btnSoundID);
+            this.Controls.Add(this.btnUserTimer);
+            this.Controls.Add(this.btnValSet);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
@@ -46,6 +90,11 @@ namespace SlotEffectMaker2023
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnValSet;
+        private System.Windows.Forms.Button btnSoundID;
+        private System.Windows.Forms.Button btnSoundPlayData;
+        private System.Windows.Forms.Button btnUserTimer;
     }
 }
 
