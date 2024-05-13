785e7cf8b1c546aeaf1e7f68a0910f730b4ff106
sin spark <spaaaark.sin120@gmail.com>
2024-05-13 15:56:13 +0900

    [FIX]メニュー画面に仕様メモを追加
    仕様を忘れないための暫定対策的なところはあるが、とりあえずこれで進む

--- SlotEffectMaker2023/MainForm.Designer.cs
+++ SlotEffectMaker2023/MainForm.Designer.cs
@@ -45,6 +45,8 @@ namespace SlotEffectMaker2023
             this.button1 = new System.Windows.Forms.Button();
             this.btnHistoryConf = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnValSet
@@ -207,12 +209,32 @@ namespace SlotEffectMaker2023
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.StartModCollection);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 24);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "音再生タイミング設定:\r\nPlayerNameにBGMを入れると音量がBGM準拠となる";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "[Note]";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(549, 331);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnHistoryConf);
             this.Controls.Add(this.button1);
@@ -235,6 +257,7 @@ namespace SlotEffectMaker2023
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -256,6 +279,8 @@ namespace SlotEffectMaker2023
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnHistoryConf;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
